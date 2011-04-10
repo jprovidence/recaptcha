6cc6d0ad24608b12a4bdd6adad5420f26819db8c
niblha <nibhla@noemail.com>
2011-04-10 15:06:30 +0200

    added I18n load path to railtie configuration

--- lib/recaptcha/railtie.rb
+++ lib/recaptcha/railtie.rb
@@ -2,13 +2,18 @@ require 'recaptcha'
 module Rails
   module Recaptcha
     class Railtie < Rails::Railtie
+      
+      config.before_configuration do 
+        config.i18n.load_path += Dir[Pathname.new(File.expand_path('../', File.dirname(File.expand_path(__FILE__)))).join('locales', '*.{yml}')]
+      end
+      
       initializer "setup config" do
         begin
           ActionView::Base.send(:include, ::Recaptcha::ClientHelper)
           ActionController::Base.send(:include, ::Recaptcha::Verify)
           ActiveRecord::Base.send(:include, ::Recaptcha::ActiveRecordVerify::InstanceMethods)
         end
-      end
+      end      
     end
   end
 end
